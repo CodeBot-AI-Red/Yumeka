0bc7c3fb817826b0e5648bf05fd45588432b592b
CodeBot-AI-Red <zestfrosty@gmail.com>
2026-08-12 16:43:42 -0300

    fix auth pages duplicate header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import AppRoutes from './routes/AppRoutes'
 
+const AUTH_ROUTES = ['/login', '/registro']
+
+function AppLayout() {
+  const location = useLocation()
+  const isAuthRoute = AUTH_ROUTES.includes(location.pathname)
+
+  return (
+    <>
+      {!isAuthRoute && <Header />}
+      <AppRoutes />
+      {!isAuthRoute && <Footer />}
+    </>
+  )
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <AppRoutes />
-      <Footer />
+      <AppLayout />
     </BrowserRouter>
   )
 }
